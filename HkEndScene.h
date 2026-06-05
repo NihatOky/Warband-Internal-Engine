dc655e2094b6f4941d737702f050eff4e74a76dc
Nihat_Okyar <108963087+NihatOky@users.noreply.github.com>
2026-06-05 16:33:25 +0300

    Update HkEndScene.h

--- HkEndScene.h
+++ HkEndScene.h
@@ -2,6 +2,6 @@
 #include <d3d9.h>
 
 typedef long(__stdcall* EndScene_t)(IDirect3DDevice9* pDevice);
-extern EndScene_t oEndScene; // Sadece bildiriyoruz
+extern EndScene_t oEndScene; 
 
 long __stdcall hkEndScene(IDirect3DDevice9* pDevice);
