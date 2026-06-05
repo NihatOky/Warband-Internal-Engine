87fd5a159250f53b1a5cb28e5142064c15904411
Nihat_Okyar <108963087+NihatOky@users.noreply.github.com>
2026-06-05 16:34:09 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,7 +4,7 @@
 
 
 DWORD WINAPI MainThread(LPVOID lpParam) {
-    // Konsolu aç
+    
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
@@ -14,12 +14,12 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
     HookD3D();
     
 
-    // Döngü şartını parantez içine al ki işlem önceliği bozulmasın
+    
     while (!(GetAsyncKeyState(VK_END) & 1)) {
         Sleep(100);
     }
 
-    // Kapatmadan önce
+    
     if (f) fclose(f);
     FreeConsole();
 
@@ -27,10 +27,10 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
     return 0;
 }
 
-// D harfi büyük, geri kalanlar küçük olmalı!
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        // Thread oluşturmayı garantiye al
+      
         HANDLE hThread = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr);
         if (hThread) {
             CloseHandle(hThread);
